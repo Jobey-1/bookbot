0a4f72f15c713c0b6c7cfbeb949e25406b1846a8
Jobey <Jobe.Russ@hotmail.com>
2025-09-19 23:07:03 +1200

    fixed word count

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def get_book_text(file_path):
-    file_contents = file_path.read()
-    return file_contents
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def count_book_words(text):
+    word_count_list = text.split()
+    word_count = len(word_count_list)
+    return word_count
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        return print(get_book_text(f))
-
+    book_file_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+    num_words = count_book_words(book_text)
+    print(f"{num_words} words found in the document")
+    
 main()
